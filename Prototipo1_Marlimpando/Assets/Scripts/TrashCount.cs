a7350b998643ca4b371a5dc70136053bcaf9b42b
AgaMatt <heitormattosinho@gmail.com>
2016-08-31 16:40:30 -0300

    Cloud trash being destroyed when you get more trash (prototype)

---
+++ Prototipo1_Marlimpando/Assets/Scripts/TrashCount.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashCount : MonoBehaviour {
+
+	public GameObject trash1, trash2, trash3, trash4, trash5, trash6;
+
+	void Update()
+	{
+		if(transform.childCount > 2)
+		{
+			Destroy (trash1);
+		}
+		if(transform.childCount >4)
+		{
+			Destroy (trash2);
+		}
+		if(transform.childCount >6)
+		{
+			Destroy (trash3);
+		}
+		if(transform.childCount >8)
+		{
+			Destroy (trash4);
+		}
+		if(transform.childCount >10)
+		{
+			Destroy (trash5);
+		}
+		if(transform.childCount >12)
+		{
+			Destroy (trash6);
+		}
+
+	}
+}
