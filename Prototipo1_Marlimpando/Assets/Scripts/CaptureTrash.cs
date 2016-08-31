32e4ccb994a80fb64b52e420245b1bf11d3d8757
AgaMatt <heitormattosinho@gmail.com>
2016-08-31 16:25:39 -0300

    Getting trash and idle rotation OK

---
+++ Prototipo1_Marlimpando/Assets/Scripts/CaptureTrash.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureTrash : MonoBehaviour {
+	public GameObject player;
+	public Vector3 offset;
+	Vector3 trashOffset;
+
+	void OnTriggerEnter(Collider coll)
+	{
+		//print ("ALAA");
+		if(coll.tag == "Player"){
+			//print ("AAAAAA");
+			transform.position = player.transform.position - offset - trashOffset;
+			transform.SetParent (player.transform);
+			trashOffset = trashOffset + transform.position;
+		}
+	}
+}
---
+++ Prototipo1_Marlimpando/Assets/Scripts/RotateIdle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateIdle : MonoBehaviour {
+
+	Vector3 randomRot;
+	
+	// Update is called once per frame
+	void Update () {
+		randomRot = new Vector3(Random.Range (0, 6), Random.Range (0, 6), Random.Range(0, 6));
+		
+		transform.Rotate (randomRot);
+	}
+}
