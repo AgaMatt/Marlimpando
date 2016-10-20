b36d884cf843366b87f89b5a7fd2761da10ce91a
AgaMatt <heitormattosinho@gmail.com>
2016-10-19 22:08:21 -0200

    Better lighting

---
+++ Prototipo1_Marlimpando/Assets/Scripts/RestartAndClose.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class RestartAndClose : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (KeyCode.R))
+		{
+			SceneManager.LoadScene ("InGame");
+		}
+		if(Input.GetKey (KeyCode.P))
+		{
+			Application.Quit ();
+		}
+	}
+}
