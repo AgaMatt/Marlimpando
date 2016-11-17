2949b5b3b66264482105ea8cb6dbd9cfff3ae431
balaringabriel <balaringabriel@gmail.com>
2016-11-17 21:00:14 -0200

    MainMenu and Credits Scenes

---
+++ Prototipo1_Marlimpando/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+
+
+	public void Play ()
+	{
+		SceneManager.LoadScene ("InGame");
+	}
+
+	public void Credits ()
+	{
+		SceneManager.LoadScene ("Credits");
+	}
+
+	public void Return ()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void Exit ()
+	{
+		Application.Quit ();
+	}
+}
